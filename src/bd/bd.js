ca80d0a550b8553221366e7eb46defe68135c143
adriansanchezlarrondo <123000464+adriansanchezlarrondo@users.noreply.github.com>
2024-03-13 12:44:27 +0100

    Add files via upload

--- src/bd/bd.js
+++ src/bd/bd.js
@@ -1,10 +1,5 @@
 export const bdComentarios = [
     {
-        autor: 'Carlos Alberto',
-        fecha: '12/10/2022',
-        comentario: 'Este es un comentario Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit amet dignissimos laudantium blanditiis fuga recusandae sed culpa, earum pariatur repellat esse provident eaque totam quo sint iste, inventore deleniti quis.'
-    },
-    {
         autor: 'Jose Maria',
         fecha: '12/10/2022',
         comentario: 'Este es un comentario Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit amet dignissimos laudantium blanditiis fuga recusandae sed culpa, earum pariatur repellat esse provident eaque totam quo sint iste, inventore deleniti quis.'
@@ -14,9 +9,4 @@ export const bdComentarios = [
         fecha: '12/10/2022',
         comentario: 'Este es un comentario Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit amet dignissimos laudantium blanditiis fuga recusandae sed culpa, earum pariatur repellat esse provident eaque totam quo sint iste, inventore deleniti quis.'
     },
-    {
-        autor: 'Miguel Rodriguez',
-        fecha: '12/10/2022',
-        comentario: 'Este es un comentario Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit amet dignissimos laudantium blanditiis fuga recusandae sed culpa, earum pariatur repellat esse provident eaque totam quo sint iste, inventore deleniti quis.'
-    }
 ]
